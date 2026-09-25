b8cb43e21ffa656a8465b90a99d3e0aee2f84938
Dohyeon Kim <bandal2dev@gmail.com>
2026-09-25 11:59:49 +0900

    이미지 push를 Build image workflow로 합치고 image 역할 외 push를 저장소 정책으로 거부 (#27)

--- infra/modules/ecr-repository/main.tf
+++ infra/modules/ecr-repository/main.tf
@@ -1,6 +1,7 @@
 # 환경별 비공개 이미지 저장소다. 태그는 덮어쓸 수 없고, 최근 이미지 5개만 남긴다.
 # 이미지가 남아 있으면 저장소를 지우지 않는다. push할 때의 스캔은 계정 단위
-# 설정(modules/ecr-registry)이 맡는다.
+# 설정(modules/ecr-registry)이 맡는다. 이미지는 main의 Build image workflow가 image 역할로만
+# 올린다. 관리자 권한 사용자도 저장소 정책을 바꾸지 않고는 push할 수 없다.
 
 resource "aws_ecr_repository" "this" {
   name                 = var.name
@@ -12,6 +13,30 @@ resource "aws_ecr_repository" "this" {
   }
 
   tags = merge(var.tags, { Name = var.name })
+}
+
+data "aws_iam_policy_document" "push" {
+  statement {
+    sid     = "PushOnlyFromImageRole"
+    effect  = "Deny"
+    actions = ["ecr:CompleteLayerUpload", "ecr:InitiateLayerUpload", "ecr:PutImage", "ecr:UploadLayerPart"]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "aws:PrincipalArn"
+      values   = [var.push_role_arn]
+    }
+  }
+}
+
+resource "aws_ecr_repository_policy" "this" {
+  repository = aws_ecr_repository.this.name
+  policy     = data.aws_iam_policy_document.push.json
 }
 
 resource "aws_ecr_lifecycle_policy" "this" {
--- infra/modules/ecr-repository/variables.tf
+++ infra/modules/ecr-repository/variables.tf
@@ -3,6 +3,11 @@ variable "name" {
   description = "저장소 이름. bootstrap의 적용 역할 권한이 이 이름을 가리킨다."
 }
 
+variable "push_role_arn" {
+  type        = string
+  description = "이미지를 push할 수 있는 유일한 역할. bootstrap의 image 역할이다."
+}
+
 variable "tags" {
   type = map(string)
 }
